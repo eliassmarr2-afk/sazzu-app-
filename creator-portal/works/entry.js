f042518ee68d33771d9cceb015cedf1caf9b6592
eliassmarr2-afk <eliassmarr2@gmail.com>
2026-08-20 12:32:09 -0300

    PCI 1O: bust works module cache for signed TUS retry

--- creator-portal/works/entry.js
+++ creator-portal/works/entry.js
@@ -1,5 +1,5 @@
 import { requirePortalAccess } from '../route-guard.js';
 
 await requirePortalAccess();
-await import('./works.js');
+await import('./works.js?v=1o-tus-sign-20260820');
 await import('./rights.js');
